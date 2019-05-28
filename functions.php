0ca85d15474e4a6128d7f004babe4c8e7bb663f0
MilenaFilippova <33898241+MilenaFilippova@users.noreply.github.com>
2019-05-28 18:33:26 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,16 +10,6 @@
 		return htmlspecialchars($value);
 	}
 	
-	/*
-	function get_from_request($keys)
-	{
-		$data=[];
-		foreach($keys as $key)
-		{
-			$data[$key]=trim(array_get(get_post(),$key,''));
-		}
-		return $data;
-	}*/
 	
 	function save_file($path,$contents)
 	{
@@ -31,8 +21,3 @@
 		file_put_contents($path,$contents);
 	}
 	
-	
-	function get_post()
-	{
-		return $_POST;
-	}
